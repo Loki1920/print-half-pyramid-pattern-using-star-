95fca5a22d375dd8ca84f714016671190aa90268
LUCKY TIWARI <89370329+Loki1920@users.noreply.github.com>
2022-03-25 20:22:47 +0530

    Add files via upload

---
+++ main (2).cpp
@@ -0,0 +1,19 @@
+// print Half pyramid using star
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int row,column;
+  cout<<"enter row:";
+  cin>>row;
+
+  for(int i=1;i<=row;i++){
+      for(int j=1;j<=i;j++){
+        cout<<"*";
+      }
+  cout<<endl;
+    }
+  return 0;
+  
+}
